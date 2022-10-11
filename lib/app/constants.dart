297bdabf34c0886e9be79dbddad7cf4b6ecfeb0f
Omkar Dabade <omkar.dabade@quantello.com>
2022-10-11 13:39:02 +0530

    removed few assets

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -19,10 +19,14 @@ class Constants {
 
   static List<String> downloadUrls = [
     'https://www.africau.edu/images/default/sample.pdf',
-    'https://github.com/qt-odabade/qt_book_reader/raw/master/assets/test.pdf',
+    'https://github.com/qt-odabade/qt_book_reader/raw/master/assets/multipage.pdf',
     'https://github.com/qt-odabade/qt_book_reader/raw/master/assets/test.epub',
     'https://github.com/qt-odabade/qt_book_reader/raw/master/assets/test_2.epub',
-    'https://github.com/qt-odabade/qt_book_reader/raw/master/assets/test_3.epub',
+    'https://github.com/qt-odabade/qt_book_reader/raw/master/assets/multipage_v2.epub',
+    'http://www.daisy.org/samples/epub/valentin-hauy.epub',
+    'https://github.com/IDPF/epub3-samples/releases/download/20170606/figure-gallery-bindings.epub',
+    'https://github.com/IDPF/epub3-samples/releases/download/20170606/hefty-water.epub',
+    'http://files.infogridpacific.com/epub3/don-quijoti-epub3.epub',
     'https://github.com/qt-odabade/qt_book_reader/raw/master/assets/test.txt',
     'https://github.com/qt-odabade/qt_book_reader/raw/master/assets/test_2.txt'
   ];
